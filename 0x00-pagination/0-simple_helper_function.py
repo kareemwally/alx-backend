b03bf83b5bd4813f4c71976ed3e8f55693ebdeab
kareem <kareemwally@std.mans.edu.eg>
2024-10-28 23:29:25 +0300

    start & end indexes

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,4 +10,4 @@ def index_range(page, page_size):
     """
     start_index = (page - 1) * page_size
     end_index = page * page_size
-    return tuple(start_index, end_index)
+    return tuple((start_index, end_index))
