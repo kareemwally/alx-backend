9df9decf92f7cf0b7038e1154ace70f658648a46
kareem <kareemwally@std.mans.edu.eg>
2024-11-05 20:38:16 +0200

    language

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,23 +2,33 @@
 """
 routes
 """
-from flask import Flask
+from app import app
 from flask import render_template
 from flask_babel import Babel
 
 
-def get_locale():
-    """
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-app = Flask(__name__)
-babel = Babel(app, locale_selector=get_locale)
-
-
 class Config:
     """
-    the main class for languages
+    configuring the languagaes
     """
-    LANGUAGES = ['en', 'fr']
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """
+    returning the 1-html page
+    """
+    return render_template("1-index.html",
+                           title='Welcome to Holberton',
+                           body="Hello world")
+
+
+if __name__ == "__main__":
+    app.run()
