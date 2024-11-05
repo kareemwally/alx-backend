598be31f51c7ad54fa1debc212534f171526f204
kareem <kareemwally@std.mans.edu.eg>
2024-11-05 19:43:30 +0200

    index()

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,12 +8,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def index() -> None:
     """
     returning the specified html page
     """
     return render_template("0-index.html",
                            title='Welcome to Holberton',
                            body="Hello world")
+
+
 if __name__ == "__main__":
     app.run()
